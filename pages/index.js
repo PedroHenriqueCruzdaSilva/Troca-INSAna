19db03bf36e217e68743e66c6a9778c4a49a755f
Pedro Henrique Cruz da Silva <90793103+PedroHenriqueCruzdaSilva@users.noreply.github.com>
2023-04-26 09:12:00 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -219,12 +219,14 @@ function Timeline({ valorDoFiltro, ...props }) {
   // const playlistNames = props.playlists?.length > 0 ? Object.keys(props.playlists) : [];
 
   async function deleteVideo() {
-    const { data ,error } = await supabase.from("itens").delete().eq("id", produto.id);
-    if (error) {
-      alert(error.message);
-    } elif (data) {
-      alert("Produto Apagado com sucesso")
-    }
+    const { data ,error } = await supabase.from("itens").delete().eq("id", produto.id).then(
+      if(error){
+        alert(error.message)
+      }
+      elif(data){
+        alert("Deletado Com sucesso")
+      }
+    )
   }
 
   return (
